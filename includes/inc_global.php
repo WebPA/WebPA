5e10e67b85f614dd24dea6c6abea3766f5981791
Matthias Crauwels <matthias.crauwels@UGent.Be>
2014-10-24 20:12:21 +0200

    use utf8 locale

--- includes/inc_global.php
+++ includes/inc_global.php
@@ -10,13 +10,15 @@
  *
  */
 
+require_once(dirname(__FILE__) . '/../../config.inc.php');
+
 // Turn off warning about possible session & globals compatibility problem
 ini_set('session.bug_compat_warn', 0);
 
 // Set the correct timezone for your server.
-date_default_timezone_set('Europe/Brussels');
+date_default_timezone_set($_config['webpa']['timezone']);
 // Set up default locale for gettext (also used when user is not in a module).
-define('APP__DEFAULT_LOCALE', 'nl_BE');
+define('APP__DEFAULT_LOCALE', 'nl_BE.UTF8');
 
 /*
  * Configuration
@@ -25,37 +27,33 @@ define('APP__DEFAULT_LOCALE', 'nl_BE');
 ////
 // User configuration section
 ////
-
-define('APP__WWW', 'http://localhost/WebPA-Source');
-define('DOC__ROOT', '/srv/http/WebPA-Source/'); //must include the trailing /
-define('CUSTOM_CSS', '');  // Optional custom CSS file
-define('SESSION_NAME', 'WEBPA');
-ini_set('session.cookie_path', '/');
-
-// Add support for other separation in admin upload.
-define('APP__SEPARATION', ';');
+define('APP__WWW', $_config['webpa']['www_dir']);
+define('DOC__ROOT', $_config['webpa']['doc_root']); //must include the trailing /
+define('CUSTOM_CSS', $_config['webpa']['custom_css']);  // Optional custom CSS file
+define('SESSION_NAME', $_config['webpa']['session_name']);
+ini_set('session.cookie_path', $_config['webpa']['session_path']);
 
 // The month (1-12) in which the academic year is deemed to start (always on 1st of the month)
 define('APP__ACADEMIC_YEAR_START_MONTH', 9);
 
 //Database information
-define('APP__DB_HOST', 'localhost'); // If on a non-standard port, use this format:  <server>:<port>
-define('APP__DB_USERNAME', 'webpa_user');
-define('APP__DB_PASSWORD', 'password');
-define('APP__DB_DATABASE', 'webpa');
-define('APP__DB_TABLE_PREFIX', 'pa2_');
+define('APP__DB_HOST', $_config['db']['host']); // If on a non-standard port, use this format:  <server>:<port>
+define('APP__DB_USERNAME', $_config['db']['login']);
+define('APP__DB_PASSWORD', $_config['db']['password']);
+define('APP__DB_DATABASE', $_config['db']['database']);
+define('APP__DB_TABLE_PREFIX', $_config['db']['table_prefix']);
 
 // Contact info
-define('APP__EMAIL_HELP', 'someone@email.com');
-define('APP__EMAIL_NO_REPLY', 'no-reply@email.com');
+define('APP__EMAIL_HELP', $_config['webpa']['contact']);
+define('APP__EMAIL_NO_REPLY', $_config['webpa']['contact']);
 
 // logo
-define('APP__INST_LOGO', APP__WWW.'/images/logo.png');
-define('APP__INST_LOGO_ALT','Your institution name');
+define('APP__INST_LOGO', $_config['webpa']['logo']['url']);
+define('APP__INST_LOGO_ALT', $_config['webpa']['logo']['alt']);
 
 //the following lines are to accomodate the image size within the css file to prevent the image from over flowing the area provided
-define('APP__INST_HEIGHT', '25'); //image height in pixels
-define('APP__INST_WIDTH', '102'); //image width in pixels
+define('APP__INST_HEIGHT', $_config['webpa']['logo']['height']); //image height in pixels
+define('APP__INST_WIDTH', $_config['webpa']['logo']['width']); //image width in pixels
 
 //define whether the option to allow textual input is allowed
 /*NB. In the UK if requested any information about the student would need to be
@@ -77,22 +75,24 @@ ini_set('smtp_port','25');
 ini_set('sendmail_from','someone@email.com');
 
 //define the authentication to be used and in the order they are to be applied
-$LOGIN_AUTHENTICATORS[] = 'DB';
-// $LOGIN_AUTHENTICATORS[] = 'LDAP';
+//$LOGIN_AUTHENTICATORS[] = 'DB';
+//$LOGIN_AUTHENTICATORS[] = 'LDAP';
+$LOGIN_AUTHENTICATORS = $_config['webpa']['login_authenticators'];
 
 // LDAP settings
-define('LDAP__HOST', "kdc.lboro.ac.uk");
-define('LDAP__PORT', 3268);
-define('LDAP__USERNAME_EXT', '@lboro.ac.uk');
-define('LDAP__BASE', 'dc=lboro, dc=ac, dc=uk');
-define('LDAP__FILTER', 'name={username}*');
-define('LDAP__BINDRDN', '');
-define('LDAP__PASSWD', '');
-$LDAP__INFO_REQUIRED = array('displayname','mail','sn');
+define('LDAP__HOST', $_config['ldap']['host']);
+define('LDAP__PORT', $_config['ldap']['port']);
+define('LDAP__USERNAME_EXT', $_config['ldap']['username_ext']);
+define('LDAP__BASE', $_config['ldap']['base']);
+define('LDAP__FILTER', $_config['ldap']['filter']);
+define('LDAP__BINDRDN', $_config['ldap']['bindrdn']);
+define('LDAP__PASSWD', $_config['ldap']['passwd']);
+$LDAP__INFO_REQUIRED = $_config['ldap']['info_required'];
+
 // Name of attribute to use to check user type (via function below)
-define('LDAP__USER_TYPE_ATTRIBUTE', 'description');
-define('LDAP__DEBUG_LEVEL', 7);
-define('LDAP__AUTO_CREATE_USER', TRUE);
+define('LDAP__USER_TYPE_ATTRIBUTE', $_config['ldap']['user_type_attribute']);
+define('LDAP__DEBUG_LEVEL', 0);
+define('LDAP__AUTO_CREATE_USER', false);
 
 // define installed modules
 $INSTALLED_MODS = array();
@@ -142,7 +142,7 @@ require_once(DOC__ROOT.'includes/classes/class_ui.php');
 define ('APP__HELP_LINK','http://www.webpaproject.com/');
 
 //define the terminology presented to the student as mark, rating or score
-define('APP__MARK_TEXT', gettext('Score(s)'));
+define('APP__MARK_TEXT', 'Score(s)');
 
 // Collection owner types
 define('APP__COLLECTION_USER', 'user');
@@ -175,14 +175,14 @@ define('APP__REPORT_DECIMALS', "%01.2f");
 
 // File upload error messages
 $FILE_ERRORS = array(
-  UPLOAD_ERR_OK => gettext('There is no error, the file uploaded with success.'),
-  UPLOAD_ERR_INI_SIZE => gettext('The uploaded file exceeds the upload_max_filesize directive in php.ini.'),
-  UPLOAD_ERR_FORM_SIZE => gettext('The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.'),
-  UPLOAD_ERR_PARTIAL => gettext('The uploaded file was only partially uploaded.'),
-  UPLOAD_ERR_NO_FILE => gettext('No file was uploaded.'),
-  UPLOAD_ERR_NO_TMP_DIR => gettext('Missing a temporary folder.'),
-  UPLOAD_ERR_CANT_WRITE => gettext('Failed to write file to disk.'),
-  UPLOAD_ERR_EXTENSION => gettext('A PHP extension stopped the file upload.')
+  UPLOAD_ERR_OK => 'There is no error, the file uploaded with success.',
+  UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+  UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+  UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded.',
+  UPLOAD_ERR_NO_FILE => 'No file was uploaded.',
+  UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder.',
+  UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk.',
+  UPLOAD_ERR_EXTENSION => 'A PHP extension stopped the file upload.'
   );
 
 // Old config compatibility
@@ -251,36 +251,33 @@ if ($_user_id){
 
 }
 
+
 // If we found a module to load, load it!
 if ($_module_id){
-
-    $sql_module = 'SELECT module_id, module_code, module_title, module_lang FROM ' . APP__DB_TABLE_PREFIX . "module WHERE module_id = {$_SESSION['_module_id']}";
-    $_module = $DB->fetch_row($sql_module);
-    $_module_code = $_module['module_code'];
-    $_module_lang = $_module['module_lang'];
+  $sql_module = 'SELECT module_id, module_code, module_title, module_lang FROM ' . APP__DB_TABLE_PREFIX . "module WHERE module_id = {$_SESSION['_module_id']}";
+  $_module = $DB->fetch_row($sql_module);
+  $_module_code = $_module['module_code'];
+  $_module_lang = $_module['module_lang'];
 }
 else {
-    $_module_lang = APP__DEFAULT_LOCALE;
+  $_module_lang = APP__DEFAULT_LOCALE;
 }
 
 //// Setting up gettext.
 putenv("LANG=".$_module_lang);
 setlocale(LC_ALL, $_module_lang);
 $domain = "webpa";
-bindtextdomain($domain, DOC__ROOT."locale");
+bindtextdomain($domain, DOC__ROOT . 'locale');
 bind_textdomain_codeset($domain, "UTF-8");
 textdomain($domain);
 
 // Initialise UI Object (after setting up gettext).
 $UI = new UI($_user);
 
-
 function get_LDAP_user_type($data) {
 
-    $description_str = $data[0];
-
     //check in the string for staff
-    if(strripos ($description_str, 'staff') !== false) {
+    if(in_array('ugentEmployee', $data)) {
       $user_type = APP__USER_TYPE_TUTOR;
     } else {
       $user_type = APP__USER_TYPE_STUDENT;
