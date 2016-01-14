023c5093e9e4151facb06ac4a12d3374dac175bc
Matthias Crauwels <matthias.crauwels@ugent.be>
2016-01-14 15:55:31 +0100

    added missing ; and added comment

--- install/alter_table_module_lang.sql
+++ install/alter_table_module_lang.sql
@@ -1,1 +1,2 @@
-ALTER TABLE pa2_module ADD COLUMN `module_lang` varchar(10) NOT NULL DEFAULT 'en_US.UTF8'
+-- run this file if you are upgrading an existing installation to the i18n version.
+ALTER TABLE pa2_module ADD COLUMN `module_lang` varchar(10) NOT NULL DEFAULT 'en_US.UTF8';
