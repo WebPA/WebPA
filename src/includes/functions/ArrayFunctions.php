eb8215ed198bec160390a30222f07f7d4517be12
sephster <andrew@noexceptions.io>
2019-05-09 14:30:37 +0100

    Remove unused functions from ArrayFunctions

--- src/includes/functions/ArrayFunctions.php
+++ src/includes/functions/ArrayFunctions.php
@@ -12,70 +12,6 @@ namespace WebPA\includes\functions;
 
 class ArrayFunctions
 {
-     /*
-     * Is the array associative (almost works perfectly?)
-     *
-     * @param array $arr  The array to be checked
-     *
-     * @returns Status
-    */
-    function is_assoc($arr) {
-       return is_array($arr) && array_keys($arr)!==range(0,sizeof($arr)-1);
-    }
-
-
-    /*
-     * Unset an entire array (kill all contents)
-     *
-     * @param array $arr The array to be handled
-     *
-    */
-    function unset_array(&$arr) {
-      while (array_pop($arr) != NULL);
-    }
-
-
-    /*
-     * Implode an associative array..
-     * First it implodes the key-values =>  key + $inner_glue + value
-     * Then it implodes the array =>  key+$inner_glue+value + $outer_glue + key+$inner_glue+value ...
-     *
-     * @param ??    $inner_glue
-     * @param ??    $outer_glue
-     * @param Array   $array
-     *
-     * @returns Implode
-    */
-    function implode_assoc($inner_glue, $outer_glue, $array) {
-      $output = array();
-      foreach( $array as $key => $item ) {
-        $output[] = $key . $inner_glue . $item;
-      }
-      return implode($outer_glue, $output);
-    }
-
-    /**
-     *
-     * @param Array $arr
-     * @returns Array $new_array
-     */
-    function array_nonblank($arr) {
-
-      if (!is_array($arr)) { return array(); }
-      else {
-        $arr_count = count($arr);
-        $new_index = 0;
-        $new_array = array();
-        for($i=0; $i<$arr_count; ++$i) {
-          if (!empty($arr[$i])) {
-            $new_array[$new_index] = $arr[$i];
-            ++$new_index;
-          }
-        }
-        return $new_array;
-      }
-    }
-
     /**
      * Search for a value in the array
      *
@@ -94,28 +30,6 @@ class ArrayFunctions
           break;
         }
       }
-    }
-
-
-    /**
-     * Sort a 2D array
-     *
-     * @param Array $array
-     * @param ?? $key
-     *
-     * @return Array Sorted array
-     */
-
-    function array_sort_2D($array, $key) {
-      for ($i = 0; $i < sizeof($array); $i++) {
-           $sort_values[$i] = $array[$i][$key];
-      }
-      asort ($sort_values);
-      reset ($sort_values);
-      foreach ($sort_values as $arr_key => $arr_val) {
-             $sorted_arr[] = $array[$arr_key];
-      }
-      return $sorted_arr;
     }
 
     /**
@@ -163,24 +77,4 @@ class ArrayFunctions
       }
       return $assoc_array;
     }// /array_get_assoc()
-
-    /**
-     * Check to see if any of the elements in the array contain no data
-     *
-     * @param array $arr
-     *
-     * @return int number of element counts
-     *
-     */
-    function count_nonblank($arr) {
-      if (!is_array($arr)) { return 0; }
-      else {
-        $arr_count = count($arr);
-        $element_count = 0;
-        for($i=0; $i<$arr_count; ++$i) {
-          if (!empty($arr[$i])) { ++$element_count; }
-        }
-        return $element_count;
-      }
-    }
 }
