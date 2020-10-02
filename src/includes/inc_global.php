fdd3ffacc1de3ec59f0fc64bbe7da18ef16a47bd
Andrew Millington <andrew@noexceptions.io>
2020-10-02 12:14:32 +0100

    Update version number

--- src/includes/inc_global.php
+++ src/includes/inc_global.php
@@ -104,7 +104,7 @@ $INSTALLED_MODS = array();
 define('APP__NAME', 'WebPA OS');
 define('APP__TITLE', 'WebPA OS : Online Peer Assessment System');
 define('APP__ID', 'webpa');
-define('APP__VERSION', '3.1.0');
+define('APP__VERSION', '3.1.1');
 define('APP__DESCRIPTION','WebPA, an Open source, online peer assessment system.');
 define('APP__KEYWORDS','peer assessment, online, peer, assessment, tools, open source');
 
