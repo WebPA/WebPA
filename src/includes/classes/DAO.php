9d442df8d8238df014cceda2bc4677475fb25141
Andrew Millington <andrew@noexceptions.io>
2021-02-28 20:46:52 +0000

    remove remaining DAO properties and functions

--- src/includes/classes/DAO.php
+++ src/includes/classes/DAO.php
@@ -1,8 +1,7 @@
 <?php
+
 /**
- * Class : DAO
- *
- * This data access object is designed for MySQL only
+ * The database access object which serves a Doctrine DBAL connection.
  *
  * @copyright Loughborough University
  * @license https://www.gnu.org/licenses/gpl-3.0.en.html GPL version 3
@@ -12,45 +11,15 @@
 
 namespace WebPA\includes\classes;
 
+use Doctrine\DBAL\Driver\Connection;
 use Doctrine\DBAL\DriverManager;
 
 class DAO
 {
-    // Public Vars
-
-    // Private Vars
-    private $_host = '';        // DB Connection info
-    private $_user = '';
-    private $_password = '';
-    private $_database = '';
-    private $_persistent = false;
-
-    private $_conn = null;        // DB Connection object
-
-    private $_result_set = null;    // Contains the result set (temporarily)
-    private $_result_cols = null;   // Array of columns for the result set
-
-    private $_last_sql = null;      // Last query run
-    private $_result = [];      // Query results, as array of row objects
-
-    private $_output_type = 'ARRAY_A';  // 'ARRAY_A': Associative Array : $results[row]['field']
-    // 'ARRAY_N': Numeric Array : $results[row][col]
-    // 'ARRAY_B': Assoc + Numeric Array : use $results[row]['field'] or $results[row][col]
-
-    private $_output_type_int = MYSQLI_ASSOC;  // MYSQLI_ASSOC, MYSQLI_BOTH, MYSQLI_NUM
-
-    private $_insert_id = null;     // Last inserted id (on auto-increment columns)
-
-    private $_num_cols = null;
-    private $_num_rows = null;
-    private $_num_affected = null;
-
-    private $_last_error = null;
+    private Connection $_conn;
 
     /**
-     * CONSTRUCTOR
-     *
-     * Set database connection strings
+     * Set up the doctrine connection.
      *
      * @param string $host
      * @param string $user
@@ -74,15 +43,10 @@ class DAO
     }
 
     /**
-     * Return last inserted id (for auto-increment columns)
+     * Return the Doctrine database connection object.
      *
-     * @return integer
+     * @return Connection
      */
-    function get_insert_id()
-    {
-        return $this->_insert_id;
-    }
-
     public function getConnection()
     {
         return $this->_conn;
