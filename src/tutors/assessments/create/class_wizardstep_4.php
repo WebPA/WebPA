debe215554d03fe463e27114d18f0a055364dc79
Andrew Millington <andrew@noexceptions.io>
2022-06-30 11:32:12 +0100

    Remove advanced options toggle

--- src/tutors/assessments/create/class_wizardstep_4.php
+++ src/tutors/assessments/create/class_wizardstep_4.php
@@ -31,61 +31,34 @@ class WizardStep4
 
     public function head()
     {
-        ?>
-<script language="JavaScript" type="text/javascript">
-<!--
 
-  function body_onload() {
-  }// /body_onload()
-
-  function open_close(id) {
-    id = document.getElementById(id);
-
-      if (id.style.display == 'block' || id.style.display == '')
-          id.style.display = 'none';
-      else
-          id.style.display = 'block';
-
-      return;
-  }
-
-//-->
-</script>
-<?php
     }
 
     // /->head()
 
     public function form()
     {
-        $assessment_type = $this->wizard->get_field('assessment_type', 1); ?>
+        $assessment_type = $this->wizard->get_field('assessment_type', 0); ?>
     <h2>Assessment Type</h2>
     <div class="form_section">
       <table class="form" cellpadding="2" cellspacing="2">
       <tr>
         <td>
-          <input type="radio" name="assessment_type" value="1" id="both" <?php echo (!$assessment_type)? 'checked="checked"' : ''; ?>>
+          <input type="radio" name="assessment_type" value="1" id="both" <?php echo !$assessment_type ? 'checked="checked"' : ''; ?>>
         </td>
         <td>
           <label class="small" for="both">Self and peer assessment</label>
         </td>
       </tr>
+          <tr>
+              <td>
+                  <input type="radio" name="assessment_type" value="0" id="peer" <?php echo $assessment_type ? 'checked="checked"' : ''; ?>/>
+              </td>
+              <td>
+                  <label class="small" for="peer">Peer assessment only</label>
+              </td>
+          </tr>
       </table>
-      <br/><br/>
-      <div style="float:right"><b>Advanced Options</b> <a href="#" onclick="open_close('advanced')"><img src="../../../images/icons/advanced_options.gif" alt="view / hide advanced options"></a>
-      <br/><br/></div>
-      <div id="advanced" style="display:none;" class="advanced_options">
-      <table class="form" cellpadding="2" cellspacing="2">
-      <tr>
-        <td>
-          <input type="radio" name="assessment_type" value="0" id="peer" <?php echo ($assessment_type)? 'checked="checked"' : ''; ?>/>
-        </td>
-        <td>
-          <label class="small" for="peer">Peer assessment only</label>
-        </td>
-      </tr>
-      </table>
-      </div>
     </div>
 
 <?php
