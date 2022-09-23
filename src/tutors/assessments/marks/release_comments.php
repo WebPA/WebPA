cfcf704b77f65b3eeeb36682baa5b6a6d5cdd816
Andrew Millington <andrew@noexceptions.io>
2022-09-23 11:11:28 +0100

    Fix duplicate comment reports being generated

--- src/tutors/assessments/marks/release_comments.php
+++ src/tutors/assessments/marks/release_comments.php
@@ -30,7 +30,7 @@ if (empty($assessmentId)) {
     $errors[] = 'No assessment ID provided.';
 }
 
-// Get every user in the assessment
+// Get every user in the assessment that has at least one comment associated with them
 $userQuery =
     'SELECT             a.assessment_id, a.assessment_name, ugm.user_id, u.forename, u.email ' .
     'FROM               ' . APP__DB_TABLE_PREFIX . 'assessment a ' .
@@ -40,64 +40,65 @@ $userQuery =
     'ON                 ugm.group_id = ug.group_id ' .
     'LEFT JOIN          '. APP__DB_TABLE_PREFIX . 'user u ' .
     'ON                 u.user_id = ugm.user_id ' .
-    'WHERE              a.assessment_id = ?';
+    'LEFT JOIN          ' . APP__DB_TABLE_PREFIX . 'user_justification uj ' .
+    'ON                 a.assessment_id = uj.assessment_id ' .
+    'AND                ugm.group_id = uj.group_id ' .
+    'AND                uj.user_id = u.user_id ' .
+    'WHERE              a.assessment_id = ? ' .
+    'AND                uj.justification_text IS NOT NULL ' .
+    'GROUP BY           a.assessment_id, a.assessment_name, ugm.user_id, u.forename, u.email';
 
 try {
     $assessmentUsers = $DB
         ->getConnection()
         ->fetchAllAssociative($userQuery, [$assessmentId], [ParameterType::STRING]);
+
+    foreach ($assessmentUsers as $user) {
+        $hash = hash('sha256', $user['assessment_id'] . $user['user_id'] . random_int(0, 1000000));
+
+        $logReportIdQuery =
+            'INSERT INTO             ' . APP__DB_TABLE_PREFIX . 'user_justification_report ' .
+            'VALUES (?, ?, ?)';
+
+        $stmt = $DB->getConnection()->prepare($logReportIdQuery);
+
+        $stmt->bindValue(1, $hash);
+        $stmt->bindValue(2, $user['assessment_id']);
+        $stmt->bindValue(3, $user['user_id'], ParameterType::INTEGER);
+
+        $stmt->execute();
+
+        $email = new Email();
+
+        $body =
+            "Dear " . $user['forename'] . ", \n\n" .
+            "<a href=\"https://www-test.webpa.is.ed.ac.uk/students/assessments/reports/justification_comments.php?r=$hash\">" .
+            "Justification comments </a> for the marks you received from your peers for assessment '" .
+            $user['assessment_name'] ."' are now available for you to view, \n\n" .
+            "Many thanks,\n" .
+            "WebPA";
+
+        $email->set_to($user['email']);
+        $email->set_bcc(['christopher.mckenzie@ed.ac.uk', 'k.lyszkiewicz@ed.ac.uk', 'vanessa.mather@ed.ac.uk']);
+        $email->set_from(APP__EMAIL_NO_REPLY);
+        $email->set_subject('WebPA - Peer Feedback Comments Available');
+        $email->set_body($body);
+        $email->send();
+    }
+
+    $publishedDateQuery =
+        'INSERT INTO                 ' . APP__DB_TABLE_PREFIX . 'user_justification_publish_date ' .
+        'VALUES                      (?, NOW())';
+
+    $stmt = $DB->getConnection()->prepare($publishedDateQuery);
+
+    $stmt->bindValue(1, $user['assessment_id']);
+
+    $stmt->execute();
 } catch (\Doctrine\DBAL\Exception $e) {
     error_log('Message: ' . $e->getMessage() . ' - Trace: ' . $e->getTraceAsString());
 
-    $errors[] = 'Could not get the assessment users from the database.';
-}
-
-foreach ($assessmentUsers as $user) {
-    $hash = hash('sha256', $user['assessment_id'] . $user['user_id'] . random_int(0, 1000000));
-
-    $logReportIdQuery =
-        'INSERT INTO             ' . APP__DB_TABLE_PREFIX . 'user_justification_report ' .
-        'VALUES (?, ?, ?)';
-
-    $stmt = $DB->getConnection()->prepare($logReportIdQuery);
-
-    $stmt->bindValue(1, $hash);
-    $stmt->bindValue(2, $user['assessment_id']);
-    $stmt->bindValue(3, $user['user_id'], ParameterType::INTEGER);
-
-    $stmt->execute();
-
-    $email = new Email();
-
-    $body =
-        "Dear " . $user['forename'] . ", \n\n" .
-        "<a href=\"https://www-test.webpa.is.ed.ac.uk/students/assessments/reports/justification_comments.php?r=$hash\">" .
-        "Justification comments </a> for the marks you received from your peers for assessment '" .
-        $user['assessment_name'] ."' are now available for you to view, \n\n" .
-        "Many thanks,\n" .
-        "WebPA";
-
-    $email->set_to($user['email']);
-    $email->set_bcc(['christopher.mckenzie@ed.ac.uk', 'k.lyszkiewicz@ed.ac.uk', 'vanessa.mather@ed.ac.uk']);
-    $email->set_from(APP__EMAIL_NO_REPLY);
-    $email->set_subject('WebPA - Peer Feedback Comments Available');
-    $email->set_body($body);
-    $email->send();
-}
-
-// Close the comments edit
-$publishedDateQuery =
-    'INSERT INTO                 ' . APP__DB_TABLE_PREFIX . 'user_justification_publish_date ' .
-    'VALUES                      (?, NOW())';
-
-$stmt = $DB->getConnection()->prepare($publishedDateQuery);
-
-$stmt->bindValue(1, $user['assessment_id']);
-
-try {
-    $stmt->execute();
-} catch (\Doctrine\DBAL\Exception $e) {
-    $errors[] = 'Could not save the publish date for these reports.';
+    $errors[] = 'A database error was encountered when trying to generate the comment reports.';
 }
 
 // Begin the page
