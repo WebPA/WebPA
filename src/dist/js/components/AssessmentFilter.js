1e13dcd088ac9586c8906c5f3d43eb0f43c0b8d9
Andrew Millington <github@beep.mozmail.com>
2025-12-11 12:55:03 +0000

    remove unused packages

--- src/dist/js/components/AssessmentFilter.js
+++ src/dist/js/components/AssessmentFilter.js
@@ -1,11 +1,13 @@
-import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-import { useState } from "react";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsx_runtime_1 = require("react/jsx-runtime");
+const react_1 = require("react");
 function AssessmentFilter({ assessmentForms, introText, moduleId, }) {
-    const [search, setSearch] = useState("");
+    const [search, setSearch] = (0, react_1.useState)("");
     const filteredForms = assessmentForms.filter((item) => item.module_title.toLowerCase().includes(search.trim()));
-    return (_jsxs(_Fragment, { children: [_jsx("h2", { children: "Your assessment forms" }), _jsx("div", { className: "form_section", children: _jsx("table", { cellPadding: "0", cellSpacing: "0", children: _jsxs("tbody", { children: [_jsxs("tr", { children: [_jsx("td", { children: _jsx("label", { className: "small", htmlFor: "module-search", children: "Filter by module name" }) }), _jsx("td", { children: _jsx("input", { id: "module-search", name: "module-search", value: search, onChange: (e) => setSearch(e.target.value) }) })] }), filteredForms.map((item) => (_jsxs("tr", { children: [_jsx("td", { children: _jsx("input", { type: "radio", name: "form_id", id: `form_${item.form_id}`, value: item.form_id }) }), _jsx("td", { children: _jsxs("label", { className: "small", htmlFor: `form_${item.form_id}`, children: [item.form_name, " ", moduleId === item.module_id
+    return ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [(0, jsx_runtime_1.jsx)("h2", { children: "Your assessment forms" }), (0, jsx_runtime_1.jsx)("div", { className: "form_section", children: (0, jsx_runtime_1.jsx)("table", { cellPadding: "0", cellSpacing: "0", children: (0, jsx_runtime_1.jsxs)("tbody", { children: [(0, jsx_runtime_1.jsxs)("tr", { children: [(0, jsx_runtime_1.jsx)("td", { children: (0, jsx_runtime_1.jsx)("label", { className: "small", htmlFor: "module-search", children: "Filter by module name" }) }), (0, jsx_runtime_1.jsx)("td", { children: (0, jsx_runtime_1.jsx)("input", { id: "module-search", name: "module-search", value: search, onChange: (e) => setSearch(e.target.value) }) })] }), filteredForms.map((item) => ((0, jsx_runtime_1.jsxs)("tr", { children: [(0, jsx_runtime_1.jsx)("td", { children: (0, jsx_runtime_1.jsx)("input", { type: "radio", name: "form_id", id: `form_${item.form_id}`, value: item.form_id }) }), (0, jsx_runtime_1.jsx)("td", { children: (0, jsx_runtime_1.jsxs)("label", { className: "small", htmlFor: `form_${item.form_id}`, children: [item.form_name, " ", moduleId === item.module_id
                                                     ? ""
-                                                    : `(${item.module_title} ${item.module_code})`] }) }), _jsxs("td", { children: ["\u00A0 \u00A0 (", _jsx("a", { style: { fontWeight: "normal", fontSize: "84%" }, href: `../../forms/edit/preview_form.php?f=${item.form_id}&amp;i=${introText}`, target: "_blank", children: "preview" }), ")"] })] }, item.form_id)))] }) }) })] }));
+                                                    : `(${item.module_title} ${item.module_code})`] }) }), (0, jsx_runtime_1.jsxs)("td", { children: ["\u00A0 \u00A0 (", (0, jsx_runtime_1.jsx)("a", { style: { fontWeight: "normal", fontSize: "84%" }, href: `../../forms/edit/preview_form.php?f=${item.form_id}&amp;i=${introText}`, target: "_blank", children: "preview" }), ")"] })] }, item.form_id)))] }) }) })] }));
 }
-export default AssessmentFilter;
+exports.default = AssessmentFilter;
 //# sourceMappingURL=AssessmentFilter.js.map
