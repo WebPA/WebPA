efe9d918051fcef72e2c1ffcdde35cdf1b14444d
Andrew Millington <github@beep.mozmail.com>
2025-12-11 12:55:55 +0000

    reinstate type module

--- src/dist/js/assessment-filter.js
+++ src/dist/js/assessment-filter.js
@@ -1,12 +1,7 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const client_1 = require("react-dom/client");
-const AssessmentFilter_js_1 = __importDefault(require("./components/AssessmentFilter.js"));
+import { jsx as _jsx } from "react/jsx-runtime";
+import { createRoot } from "react-dom/client";
+import AssessmentFilter from "./components/AssessmentFilter.js";
 // Render your react component instead
-const root = (0, client_1.createRoot)(document.getElementById("react-filter"));
-root.render((0, jsx_runtime_1.jsx)(AssessmentFilter_js_1.default, { assessmentForms: assessmentForms, introText: introText, moduleId: moduleId }));
+const root = createRoot(document.getElementById("react-filter"));
+root.render(_jsx(AssessmentFilter, { assessmentForms: assessmentForms, introText: introText, moduleId: moduleId }));
 //# sourceMappingURL=assessment-filter.js.map
